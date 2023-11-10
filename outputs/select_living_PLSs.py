f76ceffaf8d112577eb59056d909fecb9c106ee1
BiancaRius <biancafaziorius@gmail.com>
2023-11-10 17:27:03 -0300

    updating file select living PLSs

--- outputs/select_living_PLSs.py
+++ outputs/select_living_PLSs.py
@@ -29,6 +29,18 @@ for file in list_files:
         # Merge the current group into the final DataFrame
         final_merged_df = pd.concat([final_merged_df, df], ignore_index=True)
 
+# Create a DataFrame with all combinations of years and group numbers
+all_years = range(1979, 2017)
+all_group_numbers = final_merged_df['GroupNumber'].unique()
+all_combinations = pd.DataFrame([(year, group_number) for year in all_years for group_number in all_group_numbers],
+                                 columns=['YEAR', 'GroupNumber'])
+# Merge the all_combinations DataFrame with the final DataFrame to fill gaps
+final_merged_df = pd.merge(all_combinations, final_merged_df, on=['YEAR', 'GroupNumber'], how='left')
+
+# Fill NaN values in columns other than 'YEAR' and 'GroupNumber'
+final_merged_df['PID'] = final_merged_df['PID'].fillna(final_merged_df['GroupNumber'])
+final_merged_df['OC'] = final_merged_df['OC'].fillna(0.0)  # Fill NaN values in 'OC' with 0
+
 # Sort the final DataFrame by the "YEAR" column in ascending order
 final_merged_df.sort_values(by=["GroupNumber", "YEAR"], inplace=True)
 
@@ -39,26 +51,62 @@ final_merged_df.drop(columns=['GroupNumber'], inplace=True)
 final_file_path = os.path.join(path_csv, "final_merged_sorted_data.csv")
 final_merged_df.to_csv(final_file_path, index=False)
 
+# Get a list of all files with .csv extension in the directory
+list_files = [file for file in os.listdir(path_csv) if file.startswith("sorted_merged_") and file.endswith(".csv")]
+
+# Delete each file
+for file in list_files:
+    file_path = os.path.join(path_csv, file)
+    os.remove(file_path)
+    
+print(f"Deleting sorted_merged_group files.")
+
+# # Merge the all_combinations DataFrame with the final DataFrame to fill gaps
+# final_merged_df = pd.merge(all_combinations, final_merged_df, on=['YEAR', 'GroupNumber'], how='left')
+
+# # Fill NaN values in columns other than 'YEAR' and 'GroupNumber' with 0
+# final_merged_df.fillna(0, inplace=True)
+
+# # Sort the final DataFrame by the "YEAR" column in ascending order
+# final_merged_df.sort_values(by=["GroupNumber", "YEAR"], inplace=True)
+
+# # Drop the temporary group number column
+# final_merged_df.drop(columns=['GroupNumber'], inplace=True)
+
+# # Save the final merged and sorted DataFrame to a new CSV file
+# final_file_path = os.path.join(path_csv, "final_merged_sorted_data.csv")
+# final_merged_df.to_csv(final_file_path, index=False)
+
+# # # Sort the final DataFrame by the "YEAR" column in ascending order
+# # final_merged_df.sort_values(by=["GroupNumber", "YEAR"], inplace=True)
+
+# # # Drop the temporary group number column
+# # final_merged_df.drop(columns=['GroupNumber'], inplace=True)
+
+# # # Save the final merged and sorted DataFrame to a new CSV file
+# # final_file_path = os.path.join(path_csv, "final_merged_sorted_data.csv")
+# # final_merged_df.to_csv(final_file_path, index=False)
 
 
-# Print the file groups
-# for group_number, files in file_groups.items():
-    # print(f"Files with group number {group_number}: {files}")
+
+# # # Print the file groups
+# # # for group_number, files in file_groups.items():
+# #     # print(f"Files with group number {group_number}: {files}")
 
 
 
 
 
 
-# # # Create a DataFrame with the extracted numeric part
-# pls_id = pd.DataFrame({"pls_id": pd.to_numeric(s)})
+# # # # Create a DataFrame with the extracted numeric part
+# # pls_id = pd.DataFrame({"pls_id": pd.to_numeric(s)})
 
-# # # Check the structure of pls_id to make sure it is as expected
-# print(pls_id.info())
+# # # # Check the structure of pls_id to make sure it is as expected
+# # print(pls_id.info())
 
-# # # Read file with all pls traits
-# pls_traits = pd.read_csv("/home/bianca/bianca/CAETE-DVM-alloc-allom/outputs/pls_attrs-3000.csv")
+# # # # Read file with all pls traits
+# # pls_traits = pd.read_csv("/home/bianca/bianca/CAETE-DVM-alloc-allom/outputs/pls_attrs-3000.csv")
 
-# # # Select only data from living pls (those in the list of files)
-# new_pls_traits = pls_traits[pls_traits['PLS_id'].isin(pls_id['pls_id'])]
+# # # # Select only data from living pls (those in the list of files)
+# # new_pls_traits = pls_traits[pls_traits['PLS_id'].isin(pls_id['pls_id'])]
 
