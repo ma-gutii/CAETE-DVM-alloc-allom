b42c0106ad16a77d6d84edc51a56e1586e1929b1
BiancaRius <biancafaziorius@gmail.com>
2024-01-10 15:26:40 -0300

    automatizing post processing

--- src/postprocessing.py
+++ src/postprocessing.py
@@ -1,8 +1,21 @@
 import pktocsv_allspins as p 
+import time_series as t
 import os
 import joblib
 import pandas as pd
 import numpy as np
+
+while True:
+    server = input('Are you running in the server? y/n ')
+
+    if server == 'y':
+        # Set the main_path accordingly for server
+        main_path = '/home/amazonfaceme/biancarius/CAETE-DVM-alloc-allom/'
+        break
+    elif server == 'n':
+        # Set the main_path accordingly for local machine
+        main_path = '/home/bianca/bianca/CAETE-DVM-alloc-allom/'
+        break
 
 run_name = input('Run name: ')
 grd = input('Which gridcell (lat-long): ')
@@ -14,7 +27,9 @@ grd_name = f"gridcell{grd}"
 start_year = 1979
 end_year   = 2017
 
-run_breaks_hist = []
+run_breaks_hist1 = []
+run_breaks_hist2 = []
+
 
 for year in range(start_year, end_year, 1):
     #Crie as datas de início e fim no formato 'YYYYMMDD'
@@ -25,15 +40,31 @@ for year in range(start_year, end_year, 1):
     spin_id = str((year - start_year) // 1 + 1).zfill(2)
 
     # Adicione a tupla à lista run_breaks_hist
-    run_breaks_hist.append((start_date, end_date, spin_id))
+    run_breaks_hist1.append((start_date, end_date, spin_id))
 
-# Exiba a lista resultante
-# print(run_breaks_hist)
+
+for year in range(start_year, end_year, 1):
+    #Crie as datas de início e fim no formato 'YYYYMMDD'
+    start_date = f"{year}0101"
+    end_date = f"{year}1231"
+ 
+
+    # Adicione a tupla à lista run_breaks_hist
+    run_breaks_hist2.append((start_date, end_date))
+
 
 # # Process spins 1 to ..
-for date_range in run_breaks_hist:
+for date_range in run_breaks_hist1:
 
     start_date, end_date, spin_id = date_range
     file = p.read_pkz(int(spin_id), run_name, grd_name )
     p.pkz2csv(file, path, grd_name, run_name, int(spin_id), date_range)
+
+# # Navigate to the specified folder to access spins
+os.chdir(f'{main_path}outputs/{run_name}/{grd_name}/')
+
+for date_range in run_breaks_hist2:
+    print('DATE RANGE=====',date_range)
+    
+    t.join(start_date, end_date, run_breaks_hist2)
 
--- src/time_series.py
+++ src/time_series.py
@@ -29,108 +29,148 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 
-# Prompt the user to determine if the script is running on a server or not
-while True:
-    server = input('Are you running in the server? y/n ')
+#Joining together all time series, dates, and spins
+def join(start_date, end_date, run_breaks_hist): # Initialize lists to store all time series, dates, and spins
+    
+    variables_to_plot = ['photo', 'ar', 'npp',  'lai', 'f5', 'evapm', 'cleaf', 'cwood', 'croot', 'csap', 'cheart', 'csto', 'wue', 'ls']
 
-    if server == 'y':
-        # Set the main_path accordingly for server
-        main_path = '/home/amazonfaceme/biancarius/CAETE-DVM-alloc-allom/'
-        break
-    elif server == 'n':
-        # Set the main_path accordingly for local machine
-        main_path = '/home/bianca/bianca/CAETE-DVM-alloc-allom/'
-        break
+    all_series = []
+    all_series_ls = []
+    all_dates = []
+    all_spins = []
+    all_data = {variable: [] for variable in variables_to_plot}
 
-# Input the run name
-run_name = input('run name: ')
-grid_xy = input('which gridcell? (lat-long)')
+    # Iterate over all available spins
+    for spin, (start_date, end_date) in enumerate(run_breaks_hist, start=1):
+        # Load data for the current spin
+        with open(f"spin{spin:02d}.pkz", 'rb') as fh:
+            dt = joblib.load(fh)
 
-# Navigate to the specified folder
-os.chdir(f'{main_path}outputs/{run_name}/gridcell{grid_xy}/')
+        # Iterate over all variables and extract time series
+        for variable in variables_to_plot:
+            variable_series = dt.get(variable, [])
+            all_data[variable].extend(variable_series)
 
-# Initialize lists to store all time series, dates, and spins
-all_series = []
-all_series_ls = []
-all_dates = []
-all_spins = []
+        # Create date index and update dates and spins lists
+        date_index = pd.date_range(start=start_date, end=end_date, freq='D')
+        all_dates.extend(date_index)
+        all_spins.extend([spin] * len(date_index))
+
+    # Create a DataFrame with time series, dates, and spin numbers for all variables
+    all_data['Date'] = all_dates
+    all_data['Spin'] = all_spins
+    df = pd.DataFrame(all_data)
+
+    # Save the DataFrame to a CSV file
+    df.to_csv('concatenated_series_all_spins.csv', index=False)
+
+    # Convert the 'Date' column to the datetime data type if it's not already
+    df['Date'] = pd.to_datetime(df['Date'])
+
+    
+# # Prompt the user to determine if the script is running on a server or not
+# while True:
+#     server = input('Are you running in the server? y/n ')
+
+#     if server == 'y':
+#         # Set the main_path accordingly for server
+#         main_path = '/home/amazonfaceme/biancarius/CAETE-DVM-alloc-allom/'
+#         break
+#     elif server == 'n':
+#         # Set the main_path accordingly for local machine
+#         main_path = '/home/bianca/bianca/CAETE-DVM-alloc-allom/'
+#         break
+
+# # Input the run name
+# run_name = input('run name: ')
+# grid_xy = input('which gridcell? (lat-long)')
+
+# # Navigate to the specified folder
+# os.chdir(f'{main_path}outputs/{run_name}/gridcell{grid_xy}/')
+
+# # Initialize lists to store all time series, dates, and spins
+# all_series = []
+# all_series_ls = []
+# all_dates = []
+# all_spins = []
 
 
-start_year = 1979
-end_year   = 2017
+# start_year = 1979
+# end_year   = 2017
 
-run_breaks_hist = []
+# run_breaks_hist = []
 
-for year in range(start_year, end_year, 1):
-    #Crie as datas de início e fim no formato 'YYYYMMDD'
-    start_date = f"{year}0101"
-    end_date = f"{year}1231"
+# for year in range(start_year, end_year, 1):
+#     #Crie as datas de início e fim no formato 'YYYYMMDD'
+#     start_date = f"{year}0101"
+#     end_date = f"{year}1231"
  
 
-    # Adicione a tupla à lista run_breaks_hist
-    run_breaks_hist.append((start_date, end_date))
+#     # Adicione a tupla à lista run_breaks_hist
+#     run_breaks_hist.append((start_date, end_date))
 
-print(run_breaks_hist)
+# print(run_breaks_hist)
 
-# variables_to_plot = ['emaxm', 'tsoil', 'photo', 'ar', 'npp', 'lai', 'rcm', 'f5', 'runom', 'evapm', 'wsoil', 'rm', 'rg', 'cleaf', 'cwood', 'croot', 'csap', 'cheart', 'csto', 'wue', 'area', 'ls']
+# # variables_to_plot = ['emaxm', 'tsoil', 'photo', 'ar', 'npp', 'lai', 'rcm', 'f5', 'runom', 'evapm', 'wsoil', 'rm', 'rg', 'cleaf', 'cwood', 'croot', 'csap', 'cheart', 'csto', 'wue', 'area', 'ls']
 
-variables_to_plot = ['photo', 'ar', 'npp',  'lai', 'f5', 'evapm', 'cleaf', 'cwood', 'croot', 'csap', 'cheart', 'csto', 'wue', 'ls']
-all_data = {variable: [] for variable in variables_to_plot}
-all_dates = []
-all_spins = []
+# variables_to_plot = ['photo', 'ar', 'npp',  'lai', 'f5', 'evapm', 'cleaf', 'cwood', 'croot', 'csap', 'cheart', 'csto', 'wue', 'ls']
+# all_data = {variable: [] for variable in variables_to_plot}
+# all_dates = []
+# all_spins = []
 
-# Initialize lists to store all time series, dates, and spins for all variables
-all_data = {variable: [] for variable in variables_to_plot}
-all_dates = []
-all_spins = []
+# # Initialize lists to store all time series, dates, and spins for all variables
+# all_data = {variable: [] for variable in variables_to_plot}
+# all_dates = []
+# all_spins = []
 
-# Iterate over all available spins
-for spin, (start_date, end_date) in enumerate(run_breaks_hist, start=1):
-    # Load data for the current spin
-    with open(f"spin{spin:02d}.pkz", 'rb') as fh:
-        dt = joblib.load(fh)
+# # Iterate over all available spins
+# for spin, (start_date, end_date) in enumerate(run_breaks_hist, start=1):
+#     # Load data for the current spin
+#     with open(f"spin{spin:02d}.pkz", 'rb') as fh:
+#         dt = joblib.load(fh)
 
-    # Iterate over all variables and extract time series
-    for variable in variables_to_plot:
-        variable_series = dt.get(variable, [])
-        all_data[variable].extend(variable_series)
+#     # Iterate over all variables and extract time series
+#     for variable in variables_to_plot:
+#         variable_series = dt.get(variable, [])
+#         all_data[variable].extend(variable_series)
 
-    # Create date index and update dates and spins lists
-    date_index = pd.date_range(start=start_date, end=end_date, freq='D')
-    all_dates.extend(date_index)
-    all_spins.extend([spin] * len(date_index))
+#     # Create date index and update dates and spins lists
+#     date_index = pd.date_range(start=start_date, end=end_date, freq='D')
+#     all_dates.extend(date_index)
+#     all_spins.extend([spin] * len(date_index))
 
-# Create a DataFrame with time series, dates, and spin numbers for all variables
-all_data['Date'] = all_dates
-all_data['Spin'] = all_spins
-df = pd.DataFrame(all_data)
+# # Create a DataFrame with time series, dates, and spin numbers for all variables
+# all_data['Date'] = all_dates
+# all_data['Spin'] = all_spins
+# df = pd.DataFrame(all_data)
 
-# Save the DataFrame to a CSV file
-df.to_csv('concatenated_series_all_spins.csv', index=False)
+# # Save the DataFrame to a CSV file
+# df.to_csv('concatenated_series_all_spins.csv', index=False)
 
-# Convert the 'Date' column to the datetime data type if it's not already
-df['Date'] = pd.to_datetime(df['Date'])
+# # Convert the 'Date' column to the datetime data type if it's not already
+# df['Date'] = pd.to_datetime(df['Date'])
 
 
-# Create a figure and an array of subplots based on the number of variables
-num_variables = len(variables_to_plot)
-num_rows = (num_variables + 1) // 4  # Ensure at least 1 row
-fig, axs = plt.subplots(nrows=5, ncols=3, figsize=(15, 5 * num_rows), sharex=True)
+# # Create a figure and an array of subplots based on the number of variables
+# num_variables = len(variables_to_plot)
+# num_rows = (num_variables + 1) // 4  # Ensure at least 1 row
+# fig, axs = plt.subplots(nrows=5, ncols=3, figsize=(15, 5 * num_rows), sharex=True)
 
-# Flatten the axs array to handle 1D indexing
-axs = axs.flatten()
+# # Flatten the axs array to handle 1D indexing
+# axs = axs.flatten()
 
-# Iterate over variables and plot each one
-for i, variable in enumerate(variables_to_plot):
-    axs[i].plot(df['Date'], df[variable])
-    axs[i].set_ylabel(variable)
-    axs[i].set_title(f'Time Series of {variable}')
+# # Iterate over variables and plot each one
+# for i, variable in enumerate(variables_to_plot):
+#     axs[i].plot(df['Date'], df[variable])
+#     axs[i].set_ylabel(variable)
+#     axs[i].set_title(f'Time Series of {variable}')
 
-# Adjust the layout to prevent title overlap
-plt.tight_layout()
+# # Adjust the layout to prevent title overlap
+# plt.tight_layout()
 
-# Save the plot as an image
-plt.savefig(os.path.join(f'{main_path}/outputs/{run_name}/gridcell{grid_xy}/', f'timeseries_{run_name}_all_variables.png'))
+# # Save the plot as an image
+# plt.savefig(os.path.join(f'{main_path}/outputs/{run_name}/gridcell{grid_xy}/', f'timeseries_{run_name}_all_variables.png'))
 
-# Display the subplots
-plt.show()
+# # Display the subplots
+# plt.show()
+
