1d3fa93efd95ccb50016691d64923dac07beb4d4
BiancaRius <biancafaziorius@gmail.com>
2024-01-26 10:32:00 -0300

    trying plot all locations

--- src/join_locations.py
+++ src/join_locations.py
@@ -26,18 +26,37 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('/home/amazonfaceme/biancarius/CAETE-DVM-alloc-allom/outputs/ALL_LOCATIONS/concatenated_series_ALL_LOCATIONS_30prec_3y.csv')
 
-# Criar subplots para cada variável
-plt.figure(figsize=(18, 20))
+# Lista de variáveis a serem plotadas
+variables_to_plot = ['npp', 'total_carbon', 'ls', 'evapm']
 
-# Número de linhas e colunas
-num_rows = 1
-num_cols = 1
+for variable in variables_to_plot:
+    # Número de linhas e colunas
+    num_rows = len(set(df['location']))
+    num_cols = 1
 
-# Criar subplots para cada variável
-fig, axes = plt.subplots(num_rows, num_cols, sharex=True)
+    # Criar subplots para cada variável
+    plt.figure(figsize=(18, 20))
 
+    # Criar subplots para cada variável
+    fig, axes = plt.subplots(num_rows, num_cols, sharex=True)
 
-for location in set(df['location']):
-    # Filtrar o DataFrame para a localização atual
-    df_loc = df[df['location'] == location]
-    
+    for i, location in enumerate(set(df['location'])):
+        # Filtrar o DataFrame para a localização atual e a variável atual
+        df_loc_var = df[(df['location'] == location) & (df[variable].notnull())]
+
+        # Plotar cada variável no subplot correspondente
+        axes[i].plot(df_loc_var['date_dateformat'], df_loc_var[variable], label=location, linewidth=0.5, alpha=0.7)
+        axes[i].set_ylabel(variable, fontsize=18)
+        axes[i].legend()
+
+    # Rotacionar os rótulos no eixo x
+    plt.xticks(rotation=45, ha='right')
+
+    # Ajustar o layout para melhor espaçamento
+    plt.tight_layout()
+
+    # Salvar o gráfico como um arquivo PNG
+    plt.savefig(f'/home/amazonfaceme/biancarius/CAETE-DVM-alloc-allom/outputs/ALL_LOCATIONS/{variable}_multiple_locations.png')
+
+    # Fechar a figura atual para criar uma nova para a próxima variável
+    plt.close()
