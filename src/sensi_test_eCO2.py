fbe2673b5fc4b50bd16dcd88d18ff906c4668322
BiancaRius <biancafaziorius@gmail.com>
2024-01-30 15:09:29 -0300

    30prec 1 year

--- src/sensi_test_eCO2.py
+++ src/sensi_test_eCO2.py
@@ -1,4 +1,4 @@
-# Copyright 2017- LabTerra
+# Copyright 2017-  LabTerra
 
 #     This program is free software: you can redistribute it and/or modify
 #     it under the terms of the GNU General Public License as published by
