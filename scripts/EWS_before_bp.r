4c5859cf156a57b75748a192b33c1a6ec8e35498
BiancaRius <biancafaziorius@gmail.com>
2024-02-05 11:29:09 -0300

    gitignore save csv

--- scripts/EWS_before_bp.r
+++ scripts/EWS_before_bp.r
@@ -1,4 +1,5 @@
 library(earlywarnings)
+library(ggplot2)
 
 #----------------------------------------------
 
@@ -24,4 +25,11 @@ df_1y_bp <- df_1y[df_1y$Date < "1987-08",]
 df_1y_bp_npp <- df_1y_bp$Monthly_NPP_Mean
 
 df_1y_bp_npp_gws <- generic_ews(df_1y_bp_npp, winsize = 10)
+# Open the PNG device for plotting
+png("plot.png", width = 8, height = 6, units = "in", res = 300)
 
+# Create or display the plot
+plot(df_1y_bp_npp_gws)
+
+# Close the PNG device to save the plot
+dev.off()
