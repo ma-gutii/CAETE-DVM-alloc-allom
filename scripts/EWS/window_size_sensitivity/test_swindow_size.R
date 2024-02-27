38ca896a41faf4e5be293d898607f78482befe68
BiancaRius <biancafaziorius@gmail.com>
2024-02-27 16:21:56 -0300

    improved script to test window size

--- scripts/EWS/window_size_sensitivity/test_swindow_size.R
+++ scripts/EWS/window_size_sensitivity/test_swindow_size.R
@@ -17,495 +17,61 @@ date_ym_arranged <- date_ym %>% arrange(date)
 
 # Display the sorted data
 date = date_ym_arranged$date
+# table_regclim = cbind(final_df_regclim, date)
 
 
 
 ## TESTING SLIDING WINDOW SIZE ##
 
 
-#----------------------------------------------
-#   precipitation reduction: 30%
-#   frequency: 1 year
-#----------------------------------------------
-
-
-# # !!!!! note this is the monthly integrated data frame!!!!!!!
-df_1y <- read.csv("/home/bianca/bianca/CAETE-DVM-alloc-allom/src/MAN_30prec_1y_monthly.csv")
-
-
-#select the variable of interest
-df_1y_npp <- df_1y$Monthly_NPP_Mean
-
-winsize = seq(15, 35, by = 5)
-
-# Create a list to store the results for each sliding window size
-results_list <- list()
-
-
-for (ws in winsize){
-  print(ws)
-  gws_1y <- generic_ews(df_1y_npp, winsize = ws, detrending = 'loess',
-                        logtransform = FALSE, interpolate = FALSE, 
-                        AR_n = FALSE, powerspectrum = FALSE)
-  # # Adicionar a coluna ws ao gws_1y
-  # gws_1y$ws <- ws
-  # 
-  # print(head(gws_1y))
-  # dev.off()
-  # Store the results in the list
-  results_list[[paste0("ws_", ws)]] <- gws_1y
+# Function to perform common operations
+process_data <- function(file_path) {
+  df <- read.csv(file_path)
+  df_npp <- df$Monthly_NPP_Mean
   
+  winsize <- seq(15, 35, by = 5)
+  
+  results_list <- lapply(winsize, function(ws) {
+    gws <- generic_ews(df_npp, winsize = ws, detrending = 'loess',
+                       logtransform = FALSE, interpolate = FALSE, 
+                       AR_n = FALSE, powerspectrum = FALSE)
+    gws$ws <- ws
+    return(gws)
+  })
+  
+  results_list <- lapply(results_list, function(result) {
+    if (result$timeindex[1] != 1) {
+      additional_rows <- data.frame(
+        timeindex = seq(1, result$timeindex[1] - 1),
+        ar1 = rep(NA, result$timeindex[1] - 1),
+        ws = rep(result$ws[1], result$timeindex[1] - 1)
+      )
+      return(bind_rows(additional_rows, result))
+    } else {
+      return(result)
+    }
+  })
+  
+  results_list <- lapply(results_list, function(result) {
+    result$new_column <- as.yearmon(paste(result$new_column, "-01", sep = ""))
+    return(result)
+  })
+  
+  final_df <- bind_rows(results_list)
+  final_df$ws <- as.factor(final_df$ws)
+  
+  distinct_colors <- brewer.pal(nlevels(final_df$ws), "Set1")
+  
+  ggplot(final_df, aes(x = timeindex, y = ar1, color = ws)) +
+    geom_line() +
+    scale_color_manual(values = distinct_colors) +
+    labs(title = "AR1 over Timeindex",
+         x = "Timeindex",
+         y = "AR1") +
+    theme_minimal()
 }
 
-# Utilizar lapply para adicionar a coluna "ws"
-results_list <- lapply(winsize, function(ws) {
-  gws_1y <- generic_ews(df_1y_npp, winsize = ws, detrending = 'loess',
-                        logtransform = FALSE, interpolate = FALSE, 
-                        AR_n = FALSE, powerspectrum = FALSE)
-  dev.off()
-  gws_1y$ws <- ws
-  return(gws_1y)
-})
-
-# Adicionar linhas adicionais se o valor inicial de timeindex não for 1
-results_list <- lapply(results_list, function(result) {
-  if (result$timeindex[1] != 1) {
-    additional_rows <- data.frame(
-      timeindex = seq(1, result$timeindex[1] - 1),
-      ar1 = rep(NA, result$timeindex[1] - 1),
-      ws = rep(result$ws[1], result$timeindex[1] - 1)
-    )
-    return(bind_rows(additional_rows, result))
-  } else {
-    return(result)
-  }
-})
-
-# Verificar novamente se o valor inicial de timeindex é igual a 1 para cada objeto em results_list
-print(lapply(results_list, function(result) {
-  if (result$timeindex[1] == 1) {
-    return("O valor inicial de timeindex é igual a 1.")
-  } else {
-    return("O valor inicial de timeindex NÃO é igual a 1.")
-  }
-}))
-
-# Adicione sua nova coluna aqui
-results_list <- lapply(results_list, function(result) {
-  result$new_column <- as.yearmon(paste(result$new_column, "-01", sep = ""))
-  return(result)
-})
-
-# Juntar todos os componentes da lista em um único dataframe
-final_df <- bind_rows(results_list)
-
-# Converter a coluna ws para um fator para garantir cores diferentes
-final_df$ws <- as.factor(final_df$ws)
-
-# Definir uma paleta de cores mais distinta
-cores_distintas <- brewer.pal(nlevels(final_df$ws), "Set1")
-
-
-# Criar um gráfico utilizando ggplot2 com a paleta de cores distinta
-ggplot(final_df, aes(x = timeindex, y = ar1, color = ws)) +
-  geom_line() +
-  scale_color_manual(values = cores_distintas) +
-  labs(title = "AR1 ao longo do Timeindex",
-       x = "Timeindex",
-       y = "AR1") +
-  theme_minimal()
-
-########################
-#----------------------------------------------
-#   precipitation reduction: 30%
-#   frequency: 3 year
-#----------------------------------------------
-
-
-# # !!!!! note this is the monthly integrated data frame!!!!!!!
-df_3y <- read.csv("/home/bianca/bianca/CAETE-DVM-alloc-allom/src/MAN_30prec_3y_monthly.csv")
-
-
-#select the variable of interest
-df_3y_npp <- df_3y$Monthly_NPP_Mean
-
-winsize = seq(15, 35, by = 5)
-
-# Create a list to store the results for each sliding window size
-results_list <- list()
-
-
-for (ws in winsize){
-  print(ws)
-  gws_3y <- generic_ews(df_3y_npp, winsize = ws, detrending = 'loess',
-                        logtransform = FALSE, interpolate = FALSE, 
-                        AR_n = FALSE, powerspectrum = FALSE)
-  # # Adicionar a coluna ws ao gws_3y
-  # gws_3y$ws <- ws
-  # 
-  # print(head(gws_3y))
-  # dev.off()
-  # Store the results in the list
-  results_list[[paste0("ws_", ws)]] <- gws_3y
-  
-}
-
-# Utilizar lapply para adicionar a coluna "ws"
-results_list <- lapply(winsize, function(ws) {
-  gws_3y <- generic_ews(df_3y_npp, winsize = ws, detrending = 'loess',
-                        logtransform = FALSE, interpolate = FALSE, 
-                        AR_n = FALSE, powerspectrum = FALSE)
-  dev.off()
-  gws_3y$ws <- ws
-  return(gws_3y)
-})
-
-# Adicionar linhas adicionais se o valor inicial de timeindex não for 1
-results_list <- lapply(results_list, function(result) {
-  if (result$timeindex[1] != 1) {
-    additional_rows <- data.frame(
-      timeindex = seq(1, result$timeindex[1] - 1),
-      ar1 = rep(NA, result$timeindex[1] - 1),
-      ws = rep(result$ws[1], result$timeindex[1] - 1)
-    )
-    return(bind_rows(additional_rows, result))
-  } else {
-    return(result)
-  }
-})
-
-# Verificar novamente se o valor inicial de timeindex é igual a 1 para cada objeto em results_list
-print(lapply(results_list, function(result) {
-  if (result$timeindex[1] == 1) {
-    return("O valor inicial de timeindex é igual a 1.")
-  } else {
-    return("O valor inicial de timeindex NÃO é igual a 1.")
-  }
-}))
-
-# Adicione sua nova coluna aqui
-results_list <- lapply(results_list, function(result) {
-  result$new_column <- as.yearmon(paste(result$new_column, "-01", sep = ""))
-  return(result)
-})
-
-# Juntar todos os componentes da lista em um único dataframe
-final_df <- bind_rows(results_list)
-
-# Converter a coluna ws para um fator para garantir cores diferentes
-final_df$ws <- as.factor(final_df$ws)
-
-# Definir uma paleta de cores mais distinta
-cores_distintas <- brewer.pal(nlevels(final_df$ws), "Set1")
-
-
-# Criar um gráfico utilizando ggplot2 com a paleta de cores distinta
-ggplot(final_df, aes(x = timeindex, y = ar1, color = ws)) +
-  geom_line() +
-  scale_color_manual(values = cores_distintas) +
-  labs(title = "AR1 ao longo do Timeindex",
-       x = "Timeindex",
-       y = "AR1") +
-  theme_minimal()
-
-#############################
-#----------------------------------------------
-#   precipitation reduction: 30%
-#   frequency: 5 year
-#----------------------------------------------
-
-
-# # !!!!! note this is the monthly integrated data frame!!!!!!!
-df_5y <- read.csv("/home/bianca/bianca/CAETE-DVM-alloc-allom/src/MAN_30prec_5y_monthly.csv")
-
-
-#select the variable of interest
-df_5y_npp <- df_5y$Monthly_NPP_Mean
-
-winsize = seq(15, 35, by = 5)
-
-# Create a list to store the results for each sliding window size
-results_list <- list()
-
-
-for (ws in winsize){
-  print(ws)
-  gws_5y <- generic_ews(df_5y_npp, winsize = ws, detrending = 'loess',
-                        logtransform = FALSE, interpolate = FALSE, 
-                        AR_n = FALSE, powerspectrum = FALSE)
-  # # Adicionar a coluna ws ao gws_5y
-  # gws_5y$ws <- ws
-  # 
-  # print(head(gws_5y))
-  # dev.off()
-  # Store the results in the list
-  results_list[[paste0("ws_", ws)]] <- gws_5y
-  
-}
-
-# Utilizar lapply para adicionar a coluna "ws"
-results_list <- lapply(winsize, function(ws) {
-  gws_5y <- generic_ews(df_5y_npp, winsize = ws, detrending = 'loess',
-                        logtransform = FALSE, interpolate = FALSE, 
-                        AR_n = FALSE, powerspectrum = FALSE)
-  dev.off()
-  gws_5y$ws <- ws
-  return(gws_5y)
-})
-
-# Adicionar linhas adicionais se o valor inicial de timeindex não for 1
-results_list <- lapply(results_list, function(result) {
-  if (result$timeindex[1] != 1) {
-    additional_rows <- data.frame(
-      timeindex = seq(1, result$timeindex[1] - 1),
-      ar1 = rep(NA, result$timeindex[1] - 1),
-      ws = rep(result$ws[1], result$timeindex[1] - 1)
-    )
-    return(bind_rows(additional_rows, result))
-  } else {
-    return(result)
-  }
-})
-
-# Verificar novamente se o valor inicial de timeindex é igual a 1 para cada objeto em results_list
-print(lapply(results_list, function(result) {
-  if (result$timeindex[1] == 1) {
-    return("O valor inicial de timeindex é igual a 1.")
-  } else {
-    return("O valor inicial de timeindex NÃO é igual a 1.")
-  }
-}))
-
-# Adicione sua nova coluna aqui
-results_list <- lapply(results_list, function(result) {
-  result$new_column <- as.yearmon(paste(result$new_column, "-01", sep = ""))
-  return(result)
-})
-
-# Juntar todos os componentes da lista em um único dataframe
-final_df <- bind_rows(results_list)
-
-# Converter a coluna ws para um fator para garantir cores diferentes
-final_df$ws <- as.factor(final_df$ws)
-
-# Definir uma paleta de cores mais distinta
-cores_distintas <- brewer.pal(nlevels(final_df$ws), "Set1")
-
-
-# Criar um gráfico utilizando ggplot2 com a paleta de cores distinta
-ggplot(final_df, aes(x = timeindex, y = ar1, color = ws)) +
-  geom_line() +
-  scale_color_manual(values = cores_distintas) +
-  labs(title = "AR1 ao longo do Timeindex",
-       x = "Timeindex",
-       y = "AR1") +
-  theme_minimal()
-
-#############################
-#----------------------------------------------
-#   precipitation reduction: 30%
-#   frequency: 7 year
-#----------------------------------------------
-
-
-# # !!!!! note this is the monthly integrated data frame!!!!!!!
-df_7y <- read.csv("/home/bianca/bianca/CAETE-DVM-alloc-allom/src/MAN_30prec_7y_monthly.csv")
-
-
-#select the variable of interest
-df_7y_npp <- df_7y$Monthly_NPP_Mean
-
-winsize = seq(15, 35, by = 5)
-
-# Create a list to store the results for each sliding window size
-results_list <- list()
-
-
-for (ws in winsize){
-  print(ws)
-  gws_7y <- generic_ews(df_7y_npp, winsize = ws, detrending = 'loess',
-                        logtransform = FALSE, interpolate = FALSE, 
-                        AR_n = FALSE, powerspectrum = FALSE)
-  # # Adicionar a coluna ws ao gws_7y
-  # gws_7y$ws <- ws
-  # 
-  # print(head(gws_7y))
-  # dev.off()
-  # Store the results in the list
-  results_list[[paste0("ws_", ws)]] <- gws_7y
-  
-}
-
-# Utilizar lapply para adicionar a coluna "ws"
-results_list <- lapply(winsize, function(ws) {
-  gws_7y <- generic_ews(df_7y_npp, winsize = ws, detrending = 'loess',
-                        logtransform = FALSE, interpolate = FALSE, 
-                        AR_n = FALSE, powerspectrum = FALSE)
-  dev.off()
-  gws_7y$ws <- ws
-  return(gws_7y)
-})
-
-# Adicionar linhas adicionais se o valor inicial de timeindex não for 1
-results_list <- lapply(results_list, function(result) {
-  if (result$timeindex[1] != 1) {
-    additional_rows <- data.frame(
-      timeindex = seq(1, result$timeindex[1] - 1),
-      ar1 = rep(NA, result$timeindex[1] - 1),
-      ws = rep(result$ws[1], result$timeindex[1] - 1)
-    )
-    return(bind_rows(additional_rows, result))
-  } else {
-    return(result)
-  }
-})
-
-# Verificar novamente se o valor inicial de timeindex é igual a 1 para cada objeto em results_list
-print(lapply(results_list, function(result) {
-  if (result$timeindex[1] == 1) {
-    return("O valor inicial de timeindex é igual a 1.")
-  } else {
-    return("O valor inicial de timeindex NÃO é igual a 1.")
-  }
-}))
-
-# Adicione sua nova coluna aqui
-results_list <- lapply(results_list, function(result) {
-  result$new_column <- as.yearmon(paste(result$new_column, "-01", sep = ""))
-  return(result)
-})
-
-# Juntar todos os componentes da lista em um único dataframe
-final_df <- bind_rows(results_list)
-
-# Converter a coluna ws para um fator para garantir cores diferentes
-final_df$ws <- as.factor(final_df$ws)
-
-# Definir uma paleta de cores mais distinta
-cores_distintas <- brewer.pal(nlevels(final_df$ws), "Set1")
-
-
-# Criar um gráfico utilizando ggplot2 com a paleta de cores distinta
-ggplot(final_df, aes(x = timeindex, y = ar1, color = ws)) +
-  geom_line() +
-  scale_color_manual(values = cores_distintas) +
-  labs(title = "AR1 ao longo do Timeindex",
-       x = "Timeindex",
-       y = "AR1") +
-  theme_minimal()
-
-df_regclim <- read.csv("/home/bianca/bianca/CAETE-DVM-alloc-allom/src/MAN_regularclimate_monthly.csv")
-
-#############################
-#----------------------------------------------
-#   precipitation reduction: 30%
-#   frequency: regular climate
-#----------------------------------------------
-
-
-# # !!!!! note this is the monthly integrated data frame!!!!!!!
-df_regclim <- read.csv("/home/bianca/bianca/CAETE-DVM-alloc-allom/src/MAN_regularclimate_monthly.csv")
-
-
-#select the variable of interest
-df_regclim_npp <- df_regclim$Monthly_NPP_Mean
-
-winsize = seq(15, 35, by = 5)
-
-# Create a list to store the results for each sliding window size
-results_list <- list()
-
-
-for (ws in winsize){
-  print(ws)
-  gws_regclim <- generic_ews(df_regclim_npp, winsize = ws, detrending = 'loess',
-                             logtransform = FALSE, interpolate = FALSE, 
-                             AR_n = FALSE, powerspectrum = FALSE)
-  # # Adicionar a coluna ws ao gws_regclim
-  # gws_regclim$ws <- ws
-  # 
-  # print(head(gws_regclim))
-  # dev.off()
-  # Store the results in the list
-  results_list[[paste0("ws_", ws)]] <- gws_regclim
-  
-}
-
-# Utilizar lapply para adicionar a coluna "ws"
-results_list <- lapply(winsize, function(ws) {
-  gws_regclim <- generic_ews(df_regclim_npp, winsize = ws, detrending = 'loess',
-                             logtransform = FALSE, interpolate = FALSE, 
-                             AR_n = FALSE, powerspectrum = FALSE)
-  dev.off()
-  gws_regclim$ws <- ws
-  return(gws_regclim)
-})
-
-# Adicionar linhas adicionais se o valor inicial de timeindex não for 1
-results_list <- lapply(results_list, function(result) {
-  if (result$timeindex[1] != 1) {
-    additional_rows <- data.frame(
-      timeindex = seq(1, result$timeindex[1] - 1),
-      ar1 = rep(NA, result$timeindex[1] - 1),
-      ws = rep(result$ws[1], result$timeindex[1] - 1)
-    )
-    return(bind_rows(additional_rows, result))
-  } else {
-    return(result)
-  }
-})
-
-# Verificar novamente se o valor inicial de timeindex é igual a 1 para cada objeto em results_list
-print(lapply(results_list, function(result) {
-  if (result$timeindex[1] == 1) {
-    return("O valor inicial de timeindex é igual a 1.")
-  } else {
-    return("O valor inicial de timeindex NÃO é igual a 1.")
-  }
-}))
-
-# Adicione sua nova coluna aqui
-results_list <- lapply(results_list, function(result) {
-  result$new_column <- as.yearmon(paste(result$new_column, "-01", sep = ""))
-  return(result)
-})
-
-# Juntar todos os componentes da lista em um único dataframe
-final_df_regclim <- bind_rows(results_list)
-
-# Converter a coluna ws para um fator para garantir cores diferentes
-final_df_regclim$ws <- as.factor(final_df$ws)
-
-table_regclim = cbind(final_df_regclim, date)
-
-# Converta a coluna 'date' para o formato de data sem o dia
-table_regclim <- table_regclim %>%
-  mutate(date = as.Date(paste(date, "01", sep = "-")),   # Adiciona o dia '01'
-         date = substr(date, 1, 7))                      # Remove o dia
-
-
-# Converter a coluna ws para um fator para garantir cores diferentes
-table_regclim$ws <- as.factor(table_regclim$ws)
-
-# Definir uma paleta de cores mais distinta
-cores_distintas <- brewer.pal(nlevels(final_df_regclim$ws), "Set1")
-
-
-# Criar um gráfico utilizando ggplot2 com a paleta de cores distinta
-ggplot(table_regclim, aes(x = timeindex, y = ar1, color = ws)) +
-  geom_line() +
-  scale_color_manual(values = cores_distintas) +
-  labs(title = "AR1 ao longo do Timeindex",
-       x = "Timeindex",
-       y = "AR1") +
-  theme_minimal()
-
-# write.csv(final_df_regclim, file =
-#             "/home/bianca/bianca/CAETE-DVM-alloc-allom/scripts/EWS/window_size_sensitivity/window_size_sensitivity_regclim.csv", row.names = FALSE)
-# 
-
-
-
-
-
+# Example usage for each file
+file_path_1y <- "/home/bianca/bianca/CAETE-DVM-alloc-allom/src/MAN_30prec_1y_monthly.csv"
+plot_1y <- process_data(file_path_1y)
 
